d525b0c855265fa2bc1ad26b7a3f161ad1437785
Neslişah Çelek <neslisah.celek@outlook.com>
2023-05-17 15:59:24 +0300

    Add comments

--- Project-2/base.py
+++ Project-2/base.py
@@ -1,22 +1,44 @@
 import plotly.express as px
 
-# Do not modify the line below.
 countries = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Falkland Islands", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"]
-
-# Do not modify the line below.
 colors = ["blue", "green", "red", "yellow"]
 
+neighbors = { # Define borders 
+    "Argentina": ["Bolivia", "Brazil", "Chile", "Paraguay", "Uruguay"],
+    "Bolivia": ["Argentina", "Brazil", "Chile", "Paraguay", "Peru"],
+    "Brazil": ["Argentina", "Bolivia", "Colombia", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"],
+    "Chile": ["Argentina", "Bolivia", "Peru"],
+    "Colombia": ["Brazil", "Ecuador", "Peru", "Venezuela"],
+    "Ecuador": ["Colombia", "Peru"],
+    "Falkland Islands": [],
+    "Guyana": ["Brazil", "Suriname", "Venezuela"],
+    "Paraguay": ["Argentina", "Bolivia", "Brazil"],
+    "Peru": ["Bolivia", "Brazil", "Chile", "Colombia", "Ecuador"],
+    "Suriname": ["Brazil", "Guyana"],
+    "Uruguay": ["Argentina", "Brazil"],
+    "Venezuela": ["Brazil", "Colombia", "Guyana"]
+}
 
-# Write your code here
-# ....................
-# ....................
-# ....................
-# ....................
-# ....................
+# Backtracking algorithm
+def backtrack(country, colormap): 
+    if country not in neighbors:
+        return True
 
+    for color in colors:
+        is_valid = True
+        for neighbor in neighbors[country]:
+            if neighbor in colormap and colormap[neighbor] == color:
+                is_valid = False
+                break
+        if is_valid:
+            colormap[country] = color
+            if backtrack(countries[countries.index(country) + 1], colormap):
+                return True
+            del colormap[country]
 
-# Do not modify this method, only call it with an appropriate argument.
-# colormap should be a dictionary having countries as keys and colors as values.
+    return False
+
+# Plot choropleth map
 def plot_choropleth(colormap):
     fig = px.choropleth(locationmode="country names",
                         locations=countries,
@@ -25,15 +47,14 @@ def plot_choropleth(colormap):
                         scope="south america")
     fig.show()
 
-
-# Implement main to call necessary functions
-if __name__ == "__main__":
-
-
-    # coloring test
     colormap_test = {"Argentina": "blue", "Bolivia": "red", "Brazil": "yellow", "Chile": "yellow", "Colombia": "red",
                      "Ecuador": "yellow", "Falkland Islands": "yellow", "Guyana": "red", "Paraguay": "green",
                      "Peru": "green", "Suriname": "green", "Uruguay": "red", "Venezuela": "green"}
 
     plot_choropleth(colormap=colormap_test)
 
+
+if __name__ == "__main__":
+    colormap = {}
+    backtrack(countries[0], colormap)
+    plot_choropleth(colormap)
